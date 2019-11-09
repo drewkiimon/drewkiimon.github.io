7d07599053c5412be1740cf27dd476efce596467
drewkiimon <andrew.r.pagan@gmail.com>
2019-11-08 20:27:00 -0800

    Creating subroutes for the blog

---
+++ src/pages/blog/test.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Navigation from "../../components/navigation"
+
+export default () => (
+  <div>
+    <Navigation></Navigation>
+    <div>This is the Test Blog</div>
+  </div>
+)
